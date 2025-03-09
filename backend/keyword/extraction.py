6d8327be3f7230a6c55c58a48c74d9429569cd2c
Marina Kaziulina <mkaziulina@mts.ai>
2025-03-09 21:48:29 +0300

    add docstrings

--- backend/keyword/extraction.py
+++ backend/keyword/extraction.py
@@ -27,17 +27,51 @@ class RelevantNer(TypedDict):
 class KeywordExtractor:
 
     def __init__(self, data: pd.DataFrame) -> None:
+        """
+        Args:
+            data (pd.DataFrame): dataset in the following format:
+                    tokens	            date	    bio_labels
+                0	[Morning, 5km...	2019-10-13	[O, O, ...
+                1	[President,  ...	2019-11-03	[B-person, ...
+                2	[", I, 've, ...	    2020-05-31	[O, O, ...
+
+        """
+
         self.entities = aggregate_entities(data)
         self.criterion = {0.05: 3.842, 
                           0.01: 6.635, 
                           0.001: 10.828}
         
     def get_category(self, entity) -> str:
+        """
+        Return most common NER label for the requested entity.
+
+        Args:
+            entity (str): Requested entity.
+
+        Returns:
+            category (str): Most common category for entity.
+        """
         return self.entities[self.entities.entity == entity].category.mode()[0]
     
     def get_frequencies(self,
                         start: datetime.datetime,
                         end: datetime.datetime) -> tuple[np.ndarray, ...]:
+        """
+        Get frequencies within and outside subcorpus.
+
+        Args:
+            start (datetime.datetime): Starting point.
+            end (datetime.datetime): Ending point.
+
+
+        Returns:
+            entities (np.ndarray): List of entities in order.
+            observed_freq_within (np.ndarray): Observed frequency.
+            freq_outside (np.ndarray): Frequency in the rest of the texts.
+            inverse_freq_within (np.ndarray): Inverse observed frequency.
+            inverse_freq_outside (np.ndarray): Inverse frequency in the rest of the texts.
+        """
         
         within = (self.entities.date > start) & (self.entities.date < end)
         frequencies_withith = self.entities[within].entity.value_counts()
@@ -59,7 +93,19 @@ class KeywordExtractor:
                          start: datetime.datetime,
                          end: datetime.datetime,
                          top: int = 10,
-                         alpha: float = 0.05) -> list[tuple[str, str]]:
+                         alpha: float = 0.05) -> list[RelevantNer]:
+        """
+        Extract top-N relevant NERs with a given alpha value.
+
+        Args:
+            start (datetime.datetime): Starting point.
+            end (datetime.datetime): Ending point.
+            top (int): Number of requested relevant NER.
+            alpha (float): Significance level.
+
+        Returns:
+            list[RelevantNer]: Requested number of relevant NERs.
+        """
         assert alpha in self.criterion.keys(), 'alpha value incorrect'
         
         entities, observed_freq_within, \
--- backend/keyword/utils.py
+++ backend/keyword/utils.py
@@ -28,17 +28,19 @@ def extract_entities(tokens: list[str], labels: list[str], date: pd.Timestamp) -
 
 def aggregate_entities(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Input in the format:
-        tokens	            date	    bio_labels
-    0	[Morning, 5km...	2019-10-13	[O, O, ...
-    1	[President,  ...	2019-11-03	[B-person, ...
-    2	[", I, 've, ...	    2020-05-31	[O, O, ...
+        Args:
+            data (pd.DataFrame): dataset in the following format:
+                    tokens	            date	    bio_labels
+                0	[Morning, 5km...	2019-10-13	[O, O, ...
+                1	[President,  ...	2019-11-03	[B-person, ...
+                2	[", I, 've, ...	    2020-05-31	[O, O, ...
 
-    Output in the format:
-        entity	                        category	date
-    0	pinkoctober	                    event	    2019-10-13
-    1	breastcancerawareness	        event	    2019-10-13
-    2	Central Park , Desa Parkcity	location	2019-10-13
+        Returns:
+            pd.DataFrame: output in the following format:
+                    entity	                        category	date
+                0	pinkoctober	                    event	    2019-10-13
+                1	breastcancerawareness	        event	    2019-10-13
+                2	Central Park , Desa Parkcity	location	2019-10-13
     """
     entities = data.apply(lambda entry: extract_entities(tokens=entry.tokens, 
                                                          labels=entry.bio_labels, 
@@ -53,11 +55,13 @@ def calculate_expected_freq(freq_within: np.ndarray,
                             inverse_freq_outside: np.ndarray) -> np.ndarray:
     """
     Calculates expected frequency
-    
     """
     total = freq_within + freq_outside + inverse_freq_within + inverse_freq_outside
     return (freq_within + freq_outside) * (freq_within + inverse_freq_within) / total
 
 
 def calculate_chi_squared(observed_freq: np.ndarray, expected_freq: np.ndarray) -> np.ndarray:
+    """
+    Calculates chi quared values
+    """
     return ((observed_freq - expected_freq) ** 2) / expected_freq
