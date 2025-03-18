fadde90455fae9cc3dc70935170455c5ac6b4604
asidorov <alexsiid29@gmail.com>
2025-03-18 23:00:37 +0300

    Replaced torch models with onnx sesions and added batched inference strategy

--- backend/kafka/tweet_processing.py
+++ backend/kafka/tweet_processing.py
@@ -3,7 +3,6 @@ import time
 from typing import Any, Dict
 from confluent_kafka import Producer, Consumer
 import pandas as pd
-import ast
 import json
 from utils import delivery_report
 
@@ -11,10 +10,10 @@ import sys
 sys.path.append('C:\\Studying\\BigBrother')
 from backend.inference.models import NerModel, ClassificationModel, BaseModel
 
-from backend.model_utils import create_model
+import os
+import psutil
+import onnxruntime
 from transformers import AutoTokenizer
-
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 logger = logging.getLogger("tweet_processing")
 logger.setLevel(logging.DEBUG)
@@ -22,6 +21,7 @@ console = logging.StreamHandler()
 console_formater = logging.Formatter("[ %(levelname)s ] %(message)s")
 console.setFormatter(console_formater)
 logger.addHandler(console)
+
 
 class TweetProcessorProducer:
     def __init__(
@@ -65,6 +65,10 @@ class TweetProcessorConsumer:
         self._tweet_processor_host = tweet_processor_host
         self._tweet_processor_port = tweet_processor_port
         self._read_topic = read_topic
+        with open('./configs/models_config.json', 'r') as config:
+            self.models_config = json.load(config)
+        self.max_batch_size = 8
+        self.patience = 10
             
     @property
     def producer(self) -> Any:
@@ -77,44 +81,35 @@ class TweetProcessorConsumer:
         _producer.connect()
 
         return _producer
+
+    def create_onnx_session(self, model_path):
+        sess_options = onnxruntime.SessionOptions()
+        sess_options.intra_op_num_threads = psutil.cpu_count(logical=True)
+
+        session = onnxruntime.InferenceSession(model_path, sess_options,
+                                               providers=["CUDAExecutionProvider", "CPUExecutionProvider"])
+        return session
     
     @property
     def models(self) -> Dict[str, BaseModel]:
-        label_2_id = {
-                 "B-corporation": 0,
-                 "B-creative_work": 1,
-                 "B-event": 2,
-                 "B-group": 3,
-                 "B-location": 4,
-                 "B-person": 5,
-                 "B-product": 6,
-                 "I-corporation": 7,
-                 "I-creative_work": 8,
-                 "I-event": 9,
-                 "I-group": 10,
-                 "I-location": 11,
-                 "I-person": 12,
-                 "I-product": 13,
-                 "O": 14
-             }
-        model_name = 'google-bert/bert-base-cased'                              
+        ner_model_name = 'google-bert/bert-base-cased'
 
-        model = create_model(model_name, label_2_id)
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, clean_up_tokenization_spaces=False)
+        tokenizer_kwargs = dict(trust_remote_code=True, clean_up_tokenization_spaces=False)
+        tokenizer = AutoTokenizer.from_pretrained(ner_model_name, **tokenizer_kwargs)
         tokenizer.add_tokens(["{{URL}}", "{{USERNAME}}", "{@", "@}"])
-        model.resize_token_embeddings(len(tokenizer))
-        ner_model = NerModel(model, tokenizer, {v: k for k, v in label_2_id.items()}, device='cuda')
 
+        ner_model = self.create_onnx_session(self.models_config['ner']['path'])
+        ner_model = NerModel(ner_model, tokenizer, self.models_config['ner']['id2label'])
 
-        model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
-        model = AutoModelForSequenceClassification.from_pretrained(model_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, clean_up_tokenization_spaces=False)
-        class_model = ClassificationModel(model, tokenizer, model.config.id2label, device='cuda')
+        sent_model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+        tokenizer = AutoTokenizer.from_pretrained(sent_model_name, **tokenizer_kwargs)
+        sent_model = self.create_onnx_session(self.models_config['sentiment']['path'])
+        sent_model = ClassificationModel(sent_model, tokenizer, self.models_config['sentiment']['id2label'])
 
-        model_name = "cardiffnlp/twitter-roberta-base-sentiment-latest"
-        model = AutoModelForSequenceClassification.from_pretrained(model_name)
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, clean_up_tokenization_spaces=False)
-        sent_model = ClassificationModel(model, tokenizer, model.config.id2label, device='cuda')
+        model_name = "lucasresck/bert-base-cased-ag-news"
+        class_model = self.create_onnx_session(self.models_config['category']['path'])
+        tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_kwargs)
+        class_model = ClassificationModel(class_model, tokenizer, self.models_config['category']['id2label'])
 
         return {
             "ner": ner_model,
@@ -135,7 +130,8 @@ class TweetProcessorConsumer:
         producer = self.producer
         models = self.models
         logger.info(f"Read data from {self._read_topic}")
-        tweet_id = 0
+        messages = []
+        last_infer = time.time()
         while True:
             msg = self._consumer.poll(10)
 
@@ -151,22 +147,22 @@ class TweetProcessorConsumer:
             df = pd.read_json(msg.value().decode('utf-8'), orient="index")
             df = df.transpose()
             raw_tweet = df["tokens"][0]
+            messages.append(raw_tweet)
 
             logger.debug(f"Raw tweet: {raw_tweet}")
-            #logger.debug(f"Raw data: {df}")
+            if len(messages) >= self.max_batch_size or (time.time() - last_infer) > self.patience:
+                data_to_send = {}
+                for name, model in models.items():
+                    data_to_send[name] = model(messages)
 
-            data_to_send = {}
-            for name, model in models.items():
-                data_to_send[name] = model(raw_tweet)
+                data_to_send["tweet"] = messages
+                logger.debug(f"Data to send to database: {data_to_send}")
+                data_to_send = json.dumps(data_to_send)
+                producer.send(data_to_send)
 
-            data_to_send["tweet"] = raw_tweet
-            data_to_send["time"] = time.time()
-            data_to_send["tweet_id"] = tweet_id
-            logger.debug(f"Data to send to database: {data_to_send}")
+                last_infer = time.time()
+                messages = []
 
-            data_to_send = json.dumps(data_to_send)
-
-            producer.send(data_to_send)
 
 if __name__ == "__main__":
     consumer = TweetProcessorConsumer(
