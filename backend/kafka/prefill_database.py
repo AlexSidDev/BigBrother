5ad7230af7ae4fb06f1150d933de58e4fc7b30e7
asidorov <alexsiid29@gmail.com>
2025-03-21 21:58:40 +0300

    Added functional to simulate that we already have some accumulated tweets for real-time statistics update demonstration

---
+++ backend/kafka/prefill_database.py
@@ -0,0 +1,35 @@
+import ast
+from datetime import datetime
+import pandas as pd
+import json
+import sys
+sys.path.append('C:\\Studying\\BigBrother')
+
+from backend.kafka.database_managers import DatabaseWriter
+
+
+class PreFiller:
+    def __init__(self,
+                 labeled_data_path: str = "data/sorted_labeled_dataset.csv",
+                 config_path: str = "configs/application_config.json"):
+        self.writer = DatabaseWriter()
+        self.data = pd.read_csv(labeled_data_path, converters={'tokens': ast.literal_eval})
+        self.data['time'] = self.data['date'].apply(PreFiller.convert_data)
+        self.data['tweet'] = self.data['tokens'].apply(' '.join)
+        self.data.drop(columns=['tokens', 'date', 'id'], inplace=True)
+        with open(config_path, 'r') as config:
+            self.start_date = PreFiller.convert_data(json.load(config)["start_date"])
+
+    @staticmethod
+    def convert_data(str_date: str):
+        return datetime.strptime(str_date, "%Y-%m-%d")
+
+    def fill(self):
+        data_to_add = self.data[self.data['time'] < self.start_date]
+        data_to_add = data_to_add.to_dict(orient='records')
+        self.writer.add_rows(data_to_add)
+
+
+if __name__ == '__main__':
+    prefiller = PreFiller()
+    prefiller.fill()
